1e97f2f2eb6737bb6c3a1482450b6ca4e3691b17
NISARGA NS <nisargans011@gmail.com>
2026-02-09 17:25:46 +0530

    Update Problem_1.py

--- Python/Problem_1.py
+++ Python/Problem_1.py
@@ -3,7 +3,7 @@
 # A sample line of code that can be executed in Python:
 print("Hello, World!")
 
-# You can just as easily store a string as a variable and then print it to stdout:
+# You can just  easily store a string in the my_string variable and then print it to stdout:
 my_string = "Hello, World!"
 print(my_string)
 # The above code will print Hello, World! on your screen (Hello, World!)
